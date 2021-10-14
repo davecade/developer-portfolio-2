f7ce573754814d2a5b59c47c99a9298a5b03dc39
Dave <dave.cadelina@outlook.com>
2021-10-14 21:23:18 +1100

    added navbar boiler plate

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+
+            </div>
+            <div className="navbar__options">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
