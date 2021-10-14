2347ebfaf179c6393bc9a75c613418dc7c4386c7
Dave <dave.cadelina@outlook.com>
2021-10-14 23:58:38 +1100

    set max width

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,16 +5,18 @@ import Tab from '../Tab/Tab'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="navbar__logo">
-                <h1>D.</h1>
+            <div className="navbar__container">
+                <div className="navbar__logo">
+                    <h1>D.</h1>
+                </div>
+                <ul className="navbar__tabs">
+                    <Tab className="home" title="Home" />
+                    <Tab className="about" title="About" />
+                    <Tab className="projects" title="Projects" />
+                    <Tab className="contact" title="Contact" />
+                    <Tab className="blog" title="Blog" />
+                </ul>
             </div>
-            <ul className="navbar__tabs">
-                <Tab className="home" title="Home" />
-                <Tab className="about" title="About" />
-                <Tab className="projects" title="Projects" />
-                <Tab className="contact" title="Contact" />
-                <Tab className="blog" title="Blog" />
-            </ul>
         </div>
     )
 }
