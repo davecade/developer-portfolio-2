29558662bc6a8b14aef40f2c192670eb2d040d9c
Dave <dave.cadelina@outlook.com>
2021-11-08 03:49:09 +1100

    hero animation done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Hero.scss'
 import profilePic from '../../assets/dave-profile-5.jpg'
 import Button from '../Button/Button'
@@ -6,10 +6,20 @@ import useOnScreen from '../OnScreen/OnScreen'
 import { connect } from 'react-redux'
 import { activateHome } from '../../redux/section/section.actions'
 
-const Hero = ({ activateHome }) => {
+const entranceStyle = {
+    transform: "translateX(0px)",
+    visibility: "visible",
+    opacity: "1"
+}
 
+const Hero = ({ activateHome }) => {
+    const [ loaded, setLoaded ] = useState(false)
     const ref = useRef()
     const isVisible = useOnScreen(ref)
+
+    useEffect(() => {
+        setTimeout(()=> setLoaded(true), 300)
+    }, [])
 
     useEffect(() =>{
         if(isVisible) {
@@ -19,13 +29,13 @@ const Hero = ({ activateHome }) => {
 
     return (
         <div id="home" className="hero">
-            <div className="hero__content">
+            <div className="hero__content" style={loaded ? entranceStyle : null}>
                 <h1 ref={ref} className="intro">I'm Dave</h1>
                 <p className="title">Software Developer from Australia. 
                 </p>
                 <Button className={"know-more"} title={"Know More"} link={"#about"} />
             </div>
-            <div className="profile__container">
+            <div className="profile__container" style={loaded ? entranceStyle : null}>
                 <img className="profile" src={profilePic} alt="profile" />
             </div>
             
