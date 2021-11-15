69f71e55a38671666aced9fdf77fce5232ccdfd9
Dave <dave.cadelina@outlook.com>
2021-11-16 02:35:36 +1100

    software developer title

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -58,7 +58,7 @@ const AboutMe = ({ activateAbout }) => {
                     <Heading className={"aboutme"} title={"About Me"} style={loadedHeading ? entranceStyle : null}/>
                     <div className="text__container">
                         <p className="text" style={loadedText1 ? entranceStyle : null}>
-                            I'm a Font-End developer eager to further my career in the software engineering industry. My work experience and background is mainly in the telecommunications industry, but because I was very passionate about coding, I decided to pursue a career in software development. I specialize in Web Development using ReactJS.
+                            I'm a Software developer eager to further my career in the software engineering industry. My work experience and background is mainly in the telecommunications industry, but because I was very passionate about coding, I decided to pursue a career in software development. I specialize in Web Development using ReactJS.
                         </p>
                         <p className="text" style={loadedText2 ? entranceStyle : null}>
                             Technology Stack: Firebase, Express, React, NodeJS.
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = ({ activateHome }) => {
         <div id="home" className="hero">
             <div className="hero__content" style={loaded ? entranceStyle : null}>
                 <h1 ref={ref} className="intro">I'm Dave</h1>
-                <p className="title">Front-End Developer from Australia. 
+                <p className="title">Software Developer from Australia. 
                 </p>
                 <Button className={"know-more"} title={"Know More"} link={"#about"} />
             </div>
